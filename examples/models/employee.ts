ad85984065e6839d6093a5635ec9ba70587e3e7a
Gillian Pérard <gillian.perard@gmail.com>
2019-06-20 14:10:52 +0200

    Fix comments before constructor params

--- examples/models/employee.ts
+++ examples/models/employee.ts
@@ -5,14 +5,20 @@ import { Gender } from './gender';
 @Serializable()
 export class Employee {
 
+    /**
+     * The employee's id
+     */
     @JsonProperty()
     public id: number;
+
+    /** The employee's email */
     @JsonProperty()
     public email: string;
 
     public constructor(
         @JsonProperty() public name: string,
         @JsonProperty() public gender: Gender,
+        /** The birth date of the employee */
         @JsonProperty() public readonly birthDate: Date
     ) { }
 
