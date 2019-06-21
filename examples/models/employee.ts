643224b65e87bccabf95f12ed61d6ff90dfa9b01
Hyeonsoo Lee <hyeonsoo.david.lee@gmail.com>
2019-06-21 19:56:46 +0900

    Add simple comment with @JsonProperty on constructor parameter (T/C)

--- examples/models/employee.ts
+++ examples/models/employee.ts
@@ -16,9 +16,10 @@ export class Employee {
     public email: string;
 
     public constructor(
+        // This comment works
         @JsonProperty() public name: string,
         @JsonProperty() public gender: Gender,
-        /** The birth date of the employee */
+        /** The birth date of the employee (readonly) */
         @JsonProperty() public readonly birthDate: Date
     ) { }
 
