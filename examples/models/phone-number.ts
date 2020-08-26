81f3ca2b1a05dae027b7b7454eea47c3bdd9fe50
Gillian Perard <GillianPerard@users.noreply.github.com>
2020-08-26 14:29:02 +0200

    feat: add support for dictionary

--- examples/models/phone-number.ts
+++ examples/models/phone-number.ts
@@ -4,4 +4,8 @@ import { JsonProperty, Serializable } from '../../src';
 export class PhoneNumber {
     @JsonProperty() countryCode: string;
     @JsonProperty() value: string;
+
+    constructor(phoneNumber?: string) {
+        this.value = phoneNumber;
+    }
 }
