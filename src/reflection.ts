db8cff5f6760802feca7c34897f7609eaf621c89
Gillian Pérard <GillianPerard@users.noreply.github.com>
2021-07-20 09:23:08 +0200

    refactor(json-property): remove predicate option
    use type instead

--- src/reflection.ts
+++ src/reflection.ts
@@ -8,41 +8,62 @@ export class Reflection {
     static designType = 'design:type';
     static designParamTypes = 'design:paramtypes';
 
-    static getBaseClass(target: object): { name: string } {
-        return Reflect.getPrototypeOf(target) as { name: string };
+    static getBaseClass(target: object): { name: string } | undefined {
+        return target ? (Reflect.getPrototypeOf(target) as { name: string }) : undefined;
     }
 
-    static getJsonPropertiesMetadata(target: object, name?: string): JsonPropertiesMetadata {
+    static getJsonPropertiesMetadata(
+        target: object,
+        name?: string
+    ): JsonPropertiesMetadata | undefined {
+        if (!target) {
+            return undefined;
+        }
+
         const key = `${Reflection.apiMap}${name || target.constructor.name}`;
         return Reflect.getMetadata(key, target);
     }
 
-    static getParamTypes(target: object): any {
-        return Reflect.getMetadata(Reflection.designParamTypes, target);
+    static getParamTypes(target: object): any | undefined {
+        return target ? Reflect.getMetadata(Reflection.designParamTypes, target) : undefined;
     }
 
-    static getJsonObjectMetadata(type: object): JsonObjectMetadata {
-        return Reflect.getMetadata(Reflection.apiMapJsonObject, type) as JsonObjectMetadata;
+    static getJsonObjectMetadata(type: object): JsonObjectMetadata | undefined {
+        return type
+            ? (Reflect.getMetadata(Reflection.apiMapJsonObject, type) as JsonObjectMetadata)
+            : undefined;
     }
 
-    static getType(target: object, key: string) {
-        return Reflect.getMetadata(Reflection.designType, target, key);
+    static getType(target: object, key: string): any | undefined {
+        return target ? Reflect.getMetadata(Reflection.designType, target, key) : undefined;
     }
 
     static isJsonObject(type: object): boolean {
-        return Reflect.hasOwnMetadata(Reflection.apiMapJsonObject, type);
+        return type ? Reflect.hasOwnMetadata(Reflection.apiMapJsonObject, type) : false;
     }
 
     static setJsonPropertiesMetadata(value: JsonPropertiesMetadata, target: object): void {
+        if (!target) {
+            return;
+        }
+
         const key = `${Reflection.apiMap}${target.constructor.name}`;
         Reflect.defineMetadata(key, value, target);
     }
 
     static setJsonObject(value: JsonObjectMetadata, target: object): void {
+        if (!target) {
+            return;
+        }
+
         Reflect.defineMetadata(Reflection.apiMapJsonObject, value, target);
     }
 
     static setType(type: any, target: object, key: string): void {
+        if (!target || !type) {
+            return;
+        }
+
         Reflect.defineMetadata(Reflection.designType, type, target, key);
     }
 }
